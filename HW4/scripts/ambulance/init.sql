c73e240b1d1a64775277f9a69fd11bf0cb36c3bd
Grisha1232 <grishaseleznev@yandex.ru>
2023-12-01 17:14:38 +0300

    update file

--- HW4/scripts/ambulance/init.sql
+++ HW4/scripts/ambulance/init.sql
@@ -1,1 +1,73 @@
+\c ambulance
 
+
+CREATE TABLE Station (
+    StatNr integer,
+    Name varchar,
+
+    PRIMARY KEY (StatNr)
+);
+
+CREATE TABLE StationsPersonal (
+    PersonNr integer,
+    Name varchar,
+    StatNr integer,
+
+    PRIMARY KEY (PersonNr),
+    FOREIGN KEY (StatNr)
+        REFERENCES Station (StatNr)
+);
+
+CREATE TABLE Doctor (
+    PersonNr integer,
+    Name varchar,
+    StatNr integer,
+    Area varchar,
+    Rang varchar,
+
+    PRIMARY KEY (PersonNr),
+    FOREIGN KEY (PersonNr, Name, StatNr)
+        REFERENCES StationsPersonal (PersonNr, Name, StatNr)
+);
+
+CREATE TABLE Caregivers (
+    PersonNr integer,
+    Name varchar,
+    StatNr integer,
+    Qualification varchar,
+
+    PRIMARY KEY (PersonNr),
+    FOREIGN KEY (PersonNr, Name, StatNr)
+                        REFERENCES StationsPersonal (PersonNr, Name, StatNr)
+);
+
+CREATE TABLE Room(
+    StatNr integer,
+    RoomNr integer,
+    BedsNr integer,
+
+    PRIMARY KEY (RoomNr)
+);
+
+CREATE TABLE Patient (
+    PatientNr integer,
+    Name varchar,
+    Disease varchar,
+    RoomNr integer,
+    "from" date,
+    "to" date,
+
+    PRIMARY KEY (PatientNr),
+    FOREIGN KEY (RoomNr)
+                     REFERENCES Room (RoomNr)
+);
+
+CREATE TABLE Treats (
+    PatientNr integer,
+    PersonNr integer,
+
+    FOREIGN KEY (PatientNr)
+                    REFERENCES Patient (PatientNr),
+    FOREIGN KEY (PersonNr)
+                    REFERENCES StationsPersonal (PersonNr)
+);
--- HW4/scripts/trains/init.sql
+++ HW4/scripts/trains/init.sql
@@ -1,1 +1,40 @@
+\c trains
 
+CREATE TABLE city (
+    Name varchar,
+    Region varchar,
+    PRIMARY KEY (Name, Region)
+);
+
+CREATE TABLE Station (
+    Name varchar,
+    TracksNr integer,
+    CityName varchar,
+    Region varchar,
+
+    PRIMARY KEY (Name),
+    FOREIGN KEY (CityName, Region) REFERENCES city (Name, Region)
+);
+
+CREATE TABLE Train (
+    TrainNr integer,
+    Length integer,
+    StartStationName varchar,
+    EndStationName varchar,
+
+    PRIMARY KEY (TrainNr),
+    FOREIGN KEY (StartStationName) REFERENCES Station (Name),
+    FOREIGN KEY (EndStationName) REFERENCES Station (Name)
+);
+
+CREATE TABLE Connection (
+    FromStation varchar,
+    ToStation varchar,
+    TrainNr integer,
+    Departure date,
+    Arrival date,
+
+    FOREIGN KEY (TrainNr) REFERENCES Train (TrainNr),
+    FOREIGN KEY (FromStation) REFERENCES Station (Name),
+    FOREIGN KEY (ToStation) REFERENCES Station (Name)
+);
